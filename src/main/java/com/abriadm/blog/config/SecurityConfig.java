db524e8aaec7e30acfa44dd19d646bdc0842b6c1
giveonaldo <abriansyahadam5@gmail.com>
2025-06-15 15:26:47 +0700

    little garabage code

--- src/main/java/com/abriadm/blog/config/SecurityConfig.java
+++ src/main/java/com/abriadm/blog/config/SecurityConfig.java
@@ -3,8 +3,6 @@ package com.abriadm.blog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
